a71e74ef056378d33f2fdfad51be8c45155c06f9
Tsvetomir Tsvetkov <tsvetomir@Tsvetomirs-Mac-mini.local>
2022-06-28 16:54:18 +0300

    Created basic homepage structure

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   standalone: true,
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+  styleUrls: ['./homepage.component.scss'],
+  imports: [MatCardModule],
 })
 export class HomepageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
